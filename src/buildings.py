708db0f6ff2cb7a57190f9019932697bf0d52b0f
Apurva404 <apurvajha90@gmail.com>
2023-02-24 10:55:30 -0800

    initial commit

---
+++ src/buildings.py
@@ -0,0 +1,17 @@
+from constants import Building_Type
+
+class Buildings:
+    def __init__(self, id: str, type: Building_Type) -> None:
+        self.id = id
+        self.type = type
+    
+    def getType(self, id: str) -> Building_Type:
+        # assumes that self's building type has been set
+        return self.type
+    
+    def setType(self, type: Building_Type) -> None:
+        self.type = type
+
+    
+
+
---
+++ src/constants.py
@@ -0,0 +1,23 @@
+# Defines constants used across the system
+
+from enum import Enum
+
+OUTPUT_SCHEDULE_DURATION = 5
+
+class Building_Type(Enum):
+    SINGLE_STORY = 1
+    DOUBLE_STORY = 2
+    COMMERCIAL = 3
+
+class Employee_Type(Enum):
+    CERTIFIED_INSTALLER = 1
+    INSTALLER_PENDING_CERTIFICATION = 2
+    LABORER = 3
+
+class Weekdays(Enum):
+    Monday = 1
+    Tuesday = 2
+    Wednesday = 3
+    Thursday = 4
+    Friday = 5
+
